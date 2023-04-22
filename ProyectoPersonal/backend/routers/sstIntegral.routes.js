92b0f4966b1fad718af1adfa716f7317952a58d3
SandraMurc <121523926+SandraMurc@users.noreply.github.com>
2023-04-22 11:11:18 -0500

    Delete ProyectoPersonal directory

--- ProyectoPersonal/backend/routers/sstIntegral.routes.js
+++
@@ -1,6 +0,0 @@
-const sstIntegralController = require("../controllers/sstIntegral.controller");
-
-module.exports = (app) => {
-    app.get("/", sstIntegralController.homeSstIntegral);
-    app.post("/sstIntegral/crear", sstIntegralController.crearSolicitud);
-}
